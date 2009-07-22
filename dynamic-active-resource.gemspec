efb6185f7fcf27f9bc90fb0991954145b8b01bc3
arusarh <arusarh@arusarhs-macbook-pro.local>
2009-07-22 22:18:42 +0530

    bumped the tiny version

--- dynamic-active-resource.gemspec
+++ dynamic-active-resource.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'dynamic-active-resource'
-  s.version = '0.0.1'
+  s.version = '0.0.2'
   s.author = 'asur'
   s.email = 'arusarka@gmail.com'
   s.homepage = 'http://github.com/arusarka/dynamic-active-resource/'
--- test/integration/test_dynamic_active_resource.rb
+++ test/integration/test_dynamic_active_resource.rb
@@ -1,6 +1,6 @@
 require "../../lib/dynamic_active_resource"
 
-class Card < DynamicActiveResource::Base
+class Card < DynARBase
   module ClassMethods
     def find_without_pagination(*args)
       scope = args.slice!(0)
@@ -18,4 +18,7 @@ Card.site = 'http://localhost:9090/projects/api_test'
 Card.user = 'testuser'
 Card.password = 'testuser'
 
+puts Card.find_without_pagination(:all).size
+
+Card.site = 'http://localhost:9090/projects/agile_hybrib_project'
 puts Card.find_without_pagination(:all).size
