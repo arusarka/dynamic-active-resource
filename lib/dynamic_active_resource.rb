577fa023c2eee8f39eabd77774e4851b8271506d
arusarh <arusarh@arusarhs-macbook-pro.local>
2009-07-22 20:26:50 +0530

    adding code to dynamic_active_resource.rb

--- lib/dynamic_active_resource.rb
+++ lib/dynamic_active_resource.rb
@@ -1,3 +1,19 @@
-class DynamicActiveResource
-  VERSION = '1.0.0'
+begin
+  require 'active_resource'
+rescue LoadError
+  require 'rubygems'
+  require 'active_resource'
+end
+
+$:.unshift File.expand_path(File.dirname(__FILE__))
+
+require 'dynamic_active_resource/common_class_methods'
+require 'dynamic_active_resource/helpers'
+
+module DynamicActiveResource
+  class Base < ActiveResource::Base
+    def self.inherited(subclass)
+      subclass.extend(DynamicActiveResource::CommonClassMethods)
+    end
+  end
 end
