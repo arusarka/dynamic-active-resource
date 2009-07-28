1dcdfccbf412f562c190fc6a235448b3c7991e18
arusarh <arusarh@arusarhs-macbook-pro.local>
2009-07-28 14:32:27 +0530

    changed version spec

---
+++ spec/dynamic_active_resource/version_spec.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe 'DynamicActiveResource::Version' do
+  it "should return the proper version" do
+    DynamicActiveResource::Version::to_s.should == '0.1.5'
+  end
+end
