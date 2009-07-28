9bffb76663800fb23444cccf19c7490f52e40ccb
arusarh <arusarh@arusarhs-macbook-pro.local>
2009-07-28 15:11:44 +0530

    changed the summary

--- dynamic-active-resource.gemspec
+++ dynamic-active-resource.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'dynamic-active-resource'
-  s.version = '0.1.5'
+  s.version = '0.1.6'
   s.author = 'asur'
   s.email = 'arusarka@gmail.com'
   s.homepage = 'http://github.com/arusarka/dynamic-active-resource/'
--- spec/dynamic_active_resource/version_spec.rb
+++ spec/dynamic_active_resource/version_spec.rb
@@ -2,6 +2,6 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe 'DynamicActiveResource::Version' do
   it "should return the proper version" do
-    DynamicActiveResource::Version::to_s.should == '0.1.5'
+    DynamicActiveResource::Version::to_s.should == '0.1.6'
   end
 end
